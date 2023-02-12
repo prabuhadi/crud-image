c478b03ceaaffe95358c220c81621886033b232a
prabuhadi <prabuhadiyo@gmail.com>
2023-02-12 23:56:33 +0700

    Create delete product controller and image asset

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -53,4 +53,24 @@ export const saveProduct = (req, res) => {
 
 export const updateProduct = async (req, res) => {};
 
-export const deleteProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {
+  const product = await Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!product) return res.status(404).json({ msg: "No Data Found" });
+
+  try {
+    const filepath = `./public/images/${product.image}`;
+    fs.unlinkSync(filepath);
+    await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "Product Deleted Successfuly" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
