9e8a1953debd161564a792651ce9314ae5a4e38b
prabuhadi <prabuhadiyo@gmail.com>
2023-02-13 00:29:12 +0700

    Add update product

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -51,7 +51,53 @@ export const saveProduct = (req, res) => {
   });
 };
 
-export const updateProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {
+  const product = await Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!product) return res.status(404).json({ msg: "No Data Found" });
+
+  let fileName = "";
+  if (req.files === null) {
+    fileName = product.image;
+  } else {
+    const file = req.files.file;
+    const fileSize = file.data.length;
+    const ext = path.extname(file.name);
+    fileName = file.md5 + ext;
+    const allowedType = [".png", ".jpg", ".jpeg"];
+
+    if (!allowedType.includes(ext.toLowerCase()))
+      return res.status(422).json({ msg: "Invalid Images" });
+    if (fileSize > 5000000)
+      return res.status(422).json({ msg: "Image must be less than 5 MB" });
+
+    const filepath = `./public/images/${product.image}`;
+    fs.unlinkSync(filepath);
+
+    file.mv(`./public/images/${fileName}`, (err) => {
+      if (err) return res.status(500).json({ msg: err.message });
+    });
+  }
+  const name = req.body.title;
+  const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+
+  try {
+    await Product.update(
+      { name: name, image: fileName, url: url },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json({ msg: "Product Updated Successfuly" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const deleteProduct = async (req, res) => {
   const product = await Product.findOne({
