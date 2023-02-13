c5dc5c05839b6d6ad8e826d62fde2e033857ad3b
prabuhadi <prabuhadiyo@gmail.com>
2023-02-13 17:13:46 +0700

    Modify components product

--- frontend/src/components/EditProduct.jsx
+++ frontend/src/components/EditProduct.jsx
@@ -1,59 +1,105 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
 const EditProduct = () => {
   const [title, setTitle] = useState("");
   const [file, setFile] = useState("");
   const [preview, setPreview] = useState("");
+  const { id } = useParams();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    getProductById();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getProductById = async () => {
+    const response = await axios.get(`http://localhost:5050/products/${id}`);
+    setTitle(response.data.name);
+    setFile(response.data.image);
+    setPreview(response.data.url);
+  };
+
   const loadImage = (e) => {
-    const image = e.target.files(0);
+    const image = e.target.files[0];
     setFile(image);
     setPreview(URL.createObjectURL(image));
   };
 
+  const updateProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", title);
+    try {
+      await axios.patch(`http://localhost:5050/products/${id}`, formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="columns is-centered mt-5">
-      <div className="column is-half">
-        <form>
-          <div className="field">
-            <label className="label">Product Name</label>
-            <div className="control">
-              <input
-                type="text"
-                className="input"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Product Name"
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Product Name</label>
-            <div className="control">
-              <div className="file">
-                <label className="file-label">
-                  <input
-                    type="file"
-                    className="file-input"
-                    onChange={loadImage}
-                  />
-                  <span className="fila-cta">
-                    <span className="file-label">Choose File</span>
-                  </span>
-                </label>
+    <div className="container mt-6">
+      <h2 className="is-size-4 has-text-centered">Update Product</h2>
+
+      <div className="columns is-centered mt-5">
+        <div className="column is-half">
+          <form onSubmit={updateProduct}>
+            <div className="field">
+              <label className="label">Product Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  placeholder="Product Name"
+                />
               </div>
             </div>
-          </div>
-          {preview ? (
-            <figure className="image is-128x128">
-              <img src={preview} alt="Preview Image" />
-            </figure>
-          ) : (
-            ""
-          )}
-        </form>
+
+            <div className="field">
+              <label className="label">Image</label>
+              <div className="control">
+                <div className="file">
+                  <label className="file-label">
+                    <input
+                      type="file"
+                      className="file-input"
+                      onChange={loadImage}
+                    />
+                    <span className="file-cta">
+                      <span className="file-label">Choose a file...</span>
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            {preview ? (
+              <figure className="image is-128x128">
+                <img src={preview} alt="Preview Image" />
+              </figure>
+            ) : (
+              ""
+            )}
+
+            <div className="field">
+              <div className="control">
+                <button type="submit" className="button is-success px-5 mt-5">
+                  Update
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,46 +1,71 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ProductList = () => {
-  const [product, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    getProduct();
+    getProducts();
   }, []);
 
-  const getProduct = async () => {
+  const getProducts = async () => {
     const response = await axios.get("http://localhost:5050/products");
     setProducts(response.data);
   };
+
+  const deleteProduct = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:5050/products/${productId}`);
+      getProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container mt-5">
-      <div className="columns is-multiline">
-        {product.map((product) => (
+      <h1 className="title mt-6 md-3 has-text-centered is-uppercase has-text-weight-bold">
+        Parfume Product List
+      </h1>
+      <p className="is-size-5 has-text-centered">
+        Create CRUD with input card image using Express.JS, React.JS
+      </p>
+      <Link to="/add" className="button is-success px-5">
+        Create New
+      </Link>
+      <div className="columns is-multiline mt-2">
+        {products.map((product) => (
           <div className="column is-one-quarter" key={product.id}>
             <div className="card">
               <div className="card-image">
                 <figure className="image is-4by3">
-                  <img src={product.url} alt="Placeholder image" />
+                  <img src={product.url} alt="Image" />
                 </figure>
               </div>
               <div className="card-content">
                 <div className="media">
-                  <div className="media-left"></div>
                   <div className="media-content">
                     <p className="title is-4">{product.name}</p>
-                    <p className="subtitle is-6">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Consequuntur eius suscipit ea accusantium sint vitae porro
-                      fugit perferendis soluta expedita.
-                    </p>
                   </div>
                 </div>
               </div>
 
               <footer className="card-footer">
-                <a className="card-footer-item">Edit</a>
-                <a className="card-footer-item">Delete</a>
+                <Link
+                  to={`edit/${product.id}`}
+                  className="card-footer-item button is-info m-1"
+                >
+                  Edit
+                </Link>
+                <a
+                  onClick={() => deleteProduct(product.id)}
+                  className="card-footer-item button is-danger m-1"
+                >
+                  Delete
+                </a>
               </footer>
             </div>
           </div>
